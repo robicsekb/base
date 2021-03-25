c0c9b2c6cf98b7727c108e06e9b24894cb2df892
Robicsek Bence <robicsekbenya@gmail.com>
2021-03-25 02:35:19 +0100

    JUnit test created for SpeedLimit override

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,16 +4,48 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 
 public class TrainSensorTest {
+    
+    TrainController mockController;
+    TrainUser mockUser;
+    TrainSensor sensor;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        mockController = mock(TrainController.class);
+        mockUser = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(mockController, mockUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void MaxSpeedLimit() {
+        when(mockController.getReferenceSpeed()).thenReturn(20);
+        sensor.overrideSpeedLimit(-5);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void NegativeSpeedLimit() {
+        when(mockController.getReferenceSpeed()).thenReturn(300);
+        sensor.overrideSpeedLimit(600);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void LessThanHalfSpeedLimit() {
+        when(mockController.getReferenceSpeed()).thenReturn(150);
+        sensor.overrideSpeedLimit(50);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void OKSpeedLimit() {
+        when(mockController.getReferenceSpeed()).thenReturn(250);
+        sensor.overrideSpeedLimit(260);
+        verify(mockUser, times(0)).setAlarmState(true);
     }
 }
